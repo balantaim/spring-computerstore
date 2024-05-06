223fed02cfc43774867778269322dce8dad6cced
Balantaim <martin.ned.atanasov@gmail.com>
2024-05-06 17:29:05 +0300

    Category images, Products view updated, Added fake-data.sql for test data

--- sql-scripts/fake-data.sql
+++ sql-scripts/fake-data.sql
@@ -1,8 +1,8 @@
-SELECT * FROM computer_store.categories;
+SELECT * FROM categories;
 
-INSERT INTO computer_store.categories 
+INSERT INTO categories
 (description, image_url, name)
 VALUES
-("The Central Processing Unit (CPU)", "/images/category/category-cpu.jpg", "cpu"),
-("A computer monitor is an output device that displays information in pictorial or textual form", "/images/category/category-monitors.jpg", "monitors"),
-("A video card, also known as a graphics card or graphics processing unit (GPU)", "/images/category/category-video-card.jpg", "video-cards");
+('The Central Processing Unit (CPU)', '/images/category/category-cpu.jpg', 'cpu'),
+('A computer monitor is an output device that displays information in pictorial or textual form', '/images/category/category-monitors.jpg', 'monitors'),
+('A video card, also known as a graphics card or graphics processing unit (GPU)', '/images/category/category-video-card.jpg', 'video-cards');
